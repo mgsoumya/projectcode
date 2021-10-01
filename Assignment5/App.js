17f735a4d3d41899cd1790d5bf710c2efdad4021
mgsoumya <90186413+mgsoumya@users.noreply.github.com>
2021-10-01 14:56:37 +0530

    Add files via upload

---
+++ Assignment5/App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './App.css';
+import listComponent from './Title';
+
+function App() {
+  return ()
+  {
+    <div>
+      <h1>Course List</h1>
+       </List>;
+    </div>
+  }
+}
+
+export default App;
